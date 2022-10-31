2768a96ef53f5bc487ce6dcb46912ba8f235a32d
jsnow7 <15jasnow@gmail.com>
2022-10-31 14:07:57 -0400

    update title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          North High Career Expo
+          North High Field Trip 2022
         </p>
         <a
           className="App-link"
